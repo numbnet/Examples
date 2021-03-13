bf7b8768ecf30bdf43d1f09de11ee4d012482d19
numbnet <zusyurec@gmail.com>
2021-03-13 03:50:20 +0200

    Create Exampl.ps1

---
+++ Win32/Exampl.ps1
@@ -0,0 +1,41 @@
+
+
+##====================================================
+## Добавьте это в начало сценария:
+$currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+$testadmin = $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+if ($testadmin -eq $false) {
+Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+exit $LASTEXITCODE
+}
+##====================================================
+## START сценария:
+echo "PS1 is Starting" 
+
+
+
+
+##====================================================
+## Test Windows Bit 
+if ((Get-WmiObject win32_operatingsystem | select osarchitecture).osarchitecture -like "64*")
+{
+#### --------------------
+Write "Windows x64";   # x64 bit OS
+}
+else
+{
+#### OpenSSH  --x32   
+	Write "Windows x32";   # x32 bit OS
+}
+
+
+
+##================================================
+#Invoke-WebRequest -Uri "https://github.com/PowerShell/Win32-OpenSSH/releases/download/v8.1.0.0p1-Beta/OpenSSH-Win64.zip" -OutFile "$env:Temp\OpenSSH\OpenSSH-Win64.zip";
+
+
+##================================================
+#### Unzip the files
+## Expand-Archive -Path "$env:temp\OpenSSH\OpenSSH-Win64.Zip" -DestinationPath "$env:ProgramFiles\OpenSSH\";
+
+
